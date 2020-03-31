7bd60a22f19e439d44179431761be1c1a9e3797a
tuzki <11761740@qq.com>
2020-03-31 17:48:43 +0800

    1

--- start.cmd
+++ start.cmd
@@ -1,3 +1,14 @@
+REM 先用cnpm下载,不然太慢
+REM start /wait cmd /c "cnpm install"
+
+REM 存在部分cnpm没有的electron的组件,用npm把没有的下载
+REM start /wait cmd /c "npm install"
+
+REM 构建electron和react
 start "build" cmd /c "npm run build"
+
+REM 运行react
 start cmd /c "npm run start"
+
+REM 运行electron
 start cmd /c "npm run electron-start"
